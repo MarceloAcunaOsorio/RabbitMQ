392f68a72a7964ae438be051062020e82eeea344
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-01-25 17:50:38 -0300

    se carga proyecto consumidor y publicador

---
+++ rabbit-publisher/src/test/java/com/rabbitmq/rabbit_publisher/RabbitPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbit_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
